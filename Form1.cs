982891ad0de8c3ca7e613cb4206631abbfe57f88
Sund64 <chris.cornelis.lukito@gmail.com>
2026-03-08 17:38:34 +0700

    Menambahkan tampilan GUI kalkulator~

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+namespace TP_MODUL3_103022400075
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
